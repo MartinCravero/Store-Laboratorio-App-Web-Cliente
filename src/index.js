95efcc23c56987d0dc350372e9936204aa7c14a0
Martin <martincravero123@gmail.com>
2025-11-08 16:30:56 -0300

    commit primer clase

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { getProducts } from "./api.js";
+let productList = document.querySelector('#product-list')
+
+getProducts().then((products)=>{
+    let template = "";
+    products.forEach(p => {
+        template += `
+        <div class="col">
+                    <div class="card">
+                        <img src="${p.image}" class="card-img-top" alt="${p.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${p.title}</h5>
+                            <p class="card-text">${p.description}</p>
+                        </div>
+                    </div>
+        `;    
+    });
+    productList.innerHTML = template;
+});
